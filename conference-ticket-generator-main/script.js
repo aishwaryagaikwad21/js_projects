287fdd1e67827cfbd58707e3ba8c626df2facb0f
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2025-07-16 23:37:55 +0530

    display image

--- conference-ticket-generator-main/script.js
+++ conference-ticket-generator-main/script.js
@@ -2,19 +2,39 @@ const form = document.getElementById("myForm")
 
 const avatar = document.getElementById("avatar");
 
+const profile = document.querySelector('#upload-icon');
+
 avatar.addEventListener("input",(e)=>{
     const file = e.target.files[0];
     console.log(file.size)
     const maxSize = 500*1024;
     const fileNameDisplay = document.getElementById("fileName");
     fileNameDisplay.textContent = `${file.name}`
+
+
     if (file) {
     fileNameDisplay.textContent = `${file.name}`;
 
     if (file.size > maxSize) {
-      e.target.value = ""; // ✅ This clears the file input
-      alert("❌ File size too large (Max: 500 KB)");
+      e.target.value = ""; // This clears the file input
+      alert("File size too large (Max: 500 KB)");
       fileNameDisplay.textContent = "";
+      return 
+    }
+    else{
+        profile.innerHTML = ""; //removes the icon
+        const reader = new FileReader();
+        reader.onload = function () {
+            const img = document.createElement("img");
+            img.src = reader.result;
+            img.alt = "Profile Preview";
+            img.style.width = "50px";
+            img.style.height = "50px";
+            img.style.borderRadius = "12px"; 
+            img.style.objectFit = "cover";
+            profile.appendChild(img);
+    };
+    reader.readAsDataURL(file);
     }
   } else {
     fileNameDisplay.textContent = "";
