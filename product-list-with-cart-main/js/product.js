3741e1f6aa1154a49d36580e078edfe90bf26d3f
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2025-08-05 16:36:37 +0530

    cards

--- product-list-with-cart-main/js/product.js
+++ product-list-with-cart-main/js/product.js
@@ -12,5 +12,37 @@ fetch('./data.json')
 renderProducts = (data) => {
    const deserts = document.getElementById("deserts")
    deserts.innerHTML = "";
-   
+   data.forEach(element => {
+        
+        const card = document.createElement("div");
+        card.className = "card"
+        //console.log(element.name)
+        const image = document.createElement("img")
+        image.src = element.image.thumbnail
+        card.appendChild(image)
+        
+
+        const btn = document.createElement("button")
+        btn.className = "btn"
+        btn.innerText = "Add to cart"
+        card.appendChild(btn)
+        //deserts.appendChild(btn)
+
+        const category = document.createElement("p");
+        category.className = "category"
+        category.innerHTML = element.category
+        card.appendChild(category)
+
+        const name = document.createElement("h4");
+        name.className = "name"
+        name.innerHTML = element.name
+        card.appendChild(name)
+
+        const price = document.createElement("strong");
+        price.className = "price"
+        price.innerHTML = `$${element.price}`
+        card.appendChild(price)
+
+        deserts.appendChild(card)
+   });
 }
